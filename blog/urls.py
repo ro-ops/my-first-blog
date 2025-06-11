d9c13530260b1340246273aa8b2914dbfe3e21d2
ZpktrA <romi.ant.ch@gmail.com>
2025-06-11 14:44:56 -0400

    Agregadas vistas y plantillas para el detalle del post tambien css para el sitio

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('tienda/', views.tienda, name='tienda'),
+    path('servicios/', views.servicios, name='servicios'),
+    path('contacto/', views.contacto, name='contacto'),
 ]
